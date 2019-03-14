c248bfb452adf7dd99aa1291c0d221f0afaa2f7f
visualstudio <visualstudio@230-PC37>
2019-03-14 17:00:52 -0400

    First Commit

--- C0730484-C0730462/Class1.cs
+++ C0730484-C0730462/Class1.cs
@@ -1,8 +1,83 @@
 ﻿using System;
 
 namespace C0730484_C0730462
+//Satinder Pal Singh 730484
+//Sourav Mehta 730462
 {
-    public class Class1
+    
+    public class BankAccount
     {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+        private BankAccount()
+        {
+
+        }
+
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account Frozen");
+            }
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+            m_balance -= amount;
+        }
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account Frozen");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
     }
 }
