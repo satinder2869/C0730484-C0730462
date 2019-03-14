0d84a4ee862882fef98ff27f381aba466611e1d3
visualstudio <visualstudio@230-PC37>
2019-03-14 17:03:42 -0400

    Second Commit

---
+++ BankTests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankTests
+//Satinder Pal Singh 730484
+//Sourav Mehta 730462
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+        }
+
+    }
+}
